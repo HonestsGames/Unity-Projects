a780af5f9a9e75fe5736a3dc489a521ad6b3ee88
HonestsGames <92328826+HonestsGames@users.noreply.github.com>
2021-10-12 19:59:47 +0300

    3
    UI Application

---
+++ 03 UI Application/Assets/Scenes/Sample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    bool num;
+
+    void Start()
+    {
+        Debug.Log(num);
+    }
+
+}
